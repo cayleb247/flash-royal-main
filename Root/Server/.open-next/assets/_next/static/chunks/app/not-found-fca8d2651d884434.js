d1cf1cf01934528d2f4f380255254e178782c182
BearTyree <beartyree@blueridgegrizzly.com>
2025-06-12 19:52:08 -0400

    I HATE LINUX

---
+++ Root/Server/.open-next/assets/_next/static/chunks/app/not-found-fca8d2651d884434.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[345],{4732:()=>{},8233:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,6874,23)),Promise.resolve().then(n.t.bind(n,4732,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[767,874,441,684,358],()=>s(8233)),_N_E=e.O()}]);
